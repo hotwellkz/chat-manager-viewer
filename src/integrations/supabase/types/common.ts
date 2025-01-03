3b83c037a897d81537bc22731f303aec51efb060
hotwellkz <hotwell.kz@gmail.com>
2025-01-03 15:22:01 +0600

    Create common.ts

---
+++ src/integrations/supabase/types/common.ts
@@ -0,0 +1,30 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export type Database = {
+  public: {
+    Tables: {
+      chat_history: ChatHistoryTable
+      deployed_projects: DeployedProjectsTable
+      files: FilesTable
+      profiles: ProfilesTable
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
